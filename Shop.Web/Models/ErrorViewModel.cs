4a6d1a177b8e7fd86ba080750233e6bd98b53826
hessam mousavi <hessam.mousavi99@gmail.com>
2023-10-21 12:50:58 +0330

    Implement SolidProgramming Layers And .gitignore

---
+++ Shop.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Shop.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
